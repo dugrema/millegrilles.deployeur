b8b3602f29a46889203df12f41991af0c7affba7
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-09-20 17:48:33 -0400

    Creation projet Python.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MilleGrilles.deployeur',
+    version='1.14',
+    packages=[''],
+    url='',
+    license='',
+    author='Mathieu Dugre',
+    author_email='',
+    description=''
+)
