a383714c74ab8d042e91c5519be8346b7a11b818
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-09-21 16:36:40 +0000

    Config reseau mg.

--- millegrilles/deployeur/MilleGrillesDeployeur.py
+++ millegrilles/deployeur/MilleGrillesDeployeur.py
@@ -24,6 +24,25 @@ class ConstantesEnvironnementMilleGrilles:
     REPERTOIRE_MILLEGRILLE_KEYS = '%s/keys' % REPERTOIRE_MILLEGRILLE_PKI
 
 
+class VersionMilleGrille:
+
+    def __init__(self):
+        self.__services = {}
+
+    def get_service(self, nom_service):
+        return self.__services[nom_service]
+
+class ServiceDockerConfiguration:
+
+    def __init__(self, configuration_json):
+        self.__configuration_json = configuration_json
+
+    def formatter_service(self):
+        service = {
+
+        }
+        return service
+
 class DeployeurMilleGrilles(Daemon, ModeleConfiguration):
     """
     Noeud gestionnaire d'une MilleGrille. Responsable de l'installation initiale, deploiement, entretient et healthcheck
@@ -34,6 +53,7 @@ class DeployeurMilleGrilles(Daemon, ModeleConfiguration):
         self.__stdout = '/var/logs/mg-manager.log'
         self.__stderr = '/var/logs/mg-manager.err'
         self.__docker = DockerFacade()
+        self.__millegrilles = list()
 
         Daemon.__init__(self, self.__pidfile, self.__stdout, self.__stderr)
         ModeleConfiguration.__init__(self)
@@ -44,20 +64,24 @@ class DeployeurMilleGrilles(Daemon, ModeleConfiguration):
         # Initialiser mais ne pas connecter a MQ
         super().initialiser(init_document=init_document, init_message=init_message, connecter=connecter)
 
-    def sanity_check_environnement(self):
+    def configurer_environnement_docker(self):
         """
         Verifie que l'environnement de la machine locale est viable pour demarrer MilleGrilles
         :return:
         """
-        # Verifier que /opt/millegrilles est bien cree avec les droits appropries
-
-        # Verifier que les certificats CA sont accessibles
-
         # Verifier que docker est accessible
+        self.configurer_swarm()
 
         # Verifier que les secrets sont deployes sur docker
 
-        pass
+        self.__logger.debug("Environnement docker pour millegrilles est pret")
+
+    def configurer_millegrilles(self):
+        for deployeur in self.__millegrilles:
+            deployeur.configurer()
+
+    def charger_liste_millegrilles(self):
+        self.__millegrilles.append(DeployeurDockerMilleGrille('test1', self.__docker))
 
     def sanity_check_millegrille(self, nom_millegrille):
         """
@@ -67,7 +91,7 @@ class DeployeurMilleGrilles(Daemon, ModeleConfiguration):
         """
         pass
 
-    def verifier_swarm_configure(self):
+    def configurer_swarm(self):
         swarm_info = self.__docker.get_docker_swarm_info()
         if swarm_info.get('message') is not None:
             self.__logger.info("Swarm pas configure")
@@ -82,6 +106,7 @@ class DockerFacade:
     def __init__(self):
         self.__docker_socket_path = DockerFacade.__format_unixsocket_docker('/var/run/docker.sock')
         self.__session = requests_unixsocket.Session()
+        self.__logger = logging.getLogger('%s' % self.__class__.__name__)
 
     @staticmethod
     def __format_unixsocket_docker(url):
@@ -114,17 +139,42 @@ class DockerFacade:
         if resultat.status_code != 200:
             raise Exception("Erreur initialisation docker swarm")
 
+    def configurer_reseau(self, nom_reseau):
+        reseaux = self.get('networks')
+        reseau_existe = False
+        for reseau in reseaux.json():
+            if reseau.get('Name') == nom_reseau:
+                reseau_existe = True
+                break
+
+        if not reseau_existe:
+            self.__logger.info("Configuration du reseau %s" % nom_reseau)
+            config_reseau = {
+                "Name": nom_reseau,
+                "Driver": "overlay",
+                "Attachable": True,
+            }
+            self.post('networks/create', config_reseau)
+
 
 class DeployeurDockerMilleGrille:
     """
     S'occupe d'une MilleGrille configuree sur docker.
     """
 
-    def __init__(self, nom_millegrille):
+    def __init__(self, nom_millegrille, docker: DockerFacade):
         self.__nom_millegrille = nom_millegrille
         self.__contexte = None  # Le contexte est initialise une fois que MQ actif
+        self.__docker = docker
+
+    def configurer(self):
+        nom_reseau = 'mg_%s_net' % self.__nom_millegrille
+        self.__docker.configurer_reseau(nom_reseau)
 
 
 logging.basicConfig()
 logging.getLogger('__main__').setLevel(logging.DEBUG)
-print(DeployeurMilleGrilles().verifier_swarm_configure())
+deployeur = DeployeurMilleGrilles()
+deployeur.charger_liste_millegrilles()
+deployeur.configurer_environnement_docker()
+deployeur.configurer_millegrilles()
