d5f3edc3ba367c22cf2520477362967f3c061955
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-09-21 11:40:07 -0400

    Creation classes pour gerer docker dans deployeur.

--- millegrilles/deployeur/MilleGrillesDeployeur.py
+++ millegrilles/deployeur/MilleGrillesDeployeur.py
@@ -36,17 +36,9 @@ class DeployeurMilleGrilles(Daemon, ModeleConfiguration):
         Daemon.__init__(self, self.__pidfile, self.__stdout, self.__stderr)
         ModeleConfiguration.__init__(self)
 
-        self.__docker_socket_path = DeployeurMilleGrilles.__format_url_docker('/var/run/docker.sock')
-
     def initialiser(self, init_document=False, init_message=False, connecter=False):
         # Initialiser mais ne pas connecter a MQ
         super().initialiser(init_document=init_document, init_message=init_message, connecter=connecter)
-
-    def get_docker_version(self):
-        session = requests_unixsocket.Session()
-        r = session.get('http+unix://%s/info' % self.__docker_socket_path)
-        registry_config = r.json()['RegistryConfig']
-        print(json.dumps(registry_config, indent=4))
 
     def sanity_check_environnement(self):
         """
@@ -71,9 +63,44 @@ class DeployeurMilleGrilles(Daemon, ModeleConfiguration):
         """
         pass
 
+
+class DockerFacade:
+    """
+    Facade pour les commandes de Docker
+    """
+
+    def __init__(self):
+        self.__docker_socket_path = DockerFacade.__format_unixsocket_docker('/var/run/docker.sock')
+        self.__session = requests_unixsocket.Session()
+
     @staticmethod
-    def __format_url_docker(url):
+    def __format_unixsocket_docker(url):
         return url.replace('/', '%2F')
 
+    def get_session(self, url):
+        r = self.__session.get('http+unix://%s/%s' % (self.__docker_socket_path, url))
+        return r
 
-DeployeurMilleGrilles().get_docker_version()
+    def get_docker_version(self):
+        r = self.get_session('info')
+        registry_config = r.json()['RegistryConfig']
+        docker_version = json.dumps(registry_config, indent=4)
+        return docker_version
+
+    def get_docker_swarm_info(self):
+        r = self.get_session('swarm')
+        swarm_info_str = r.json()
+        swarm_info = json.dumps(swarm_info_str, indent=4)
+        return swarm_info
+
+class DeployeurDockerMilleGrille:
+    """
+    S'occupe d'une MilleGrille configuree sur docker.
+    """
+
+    def __init__(self, nom_millegrille):
+        self.__nom_millegrille = nom_millegrille
+        self.__contexte = None  # Le contexte est initialise une fois que MQ actif
+
+
+print(DockerFacade().get_docker_swarm_info())
