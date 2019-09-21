5413f9652a8183a550d176402870ce20e7a17331
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-09-21 00:27:21 +0000

    Update fichier, copie bins.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo echo "Installation de MilleGrilles"
 source etc/paths.env
 
 installer_docker() {
-  sudo docker info
+  sudo docker info > /dev/null 2> /dev/null
   if [ $? -ne 0 ]; then
     echo "[INFO] Installation de docker"
     set -e  # Arreter execution sur erreur
@@ -33,16 +33,34 @@ preparer_opt() {
   sudo mkdir -p $MILLEGRILLES_ETC
   sudo mkdir -p $MILLEGRILLES_CACERT
 
+  sudo chmod -R 2755 $MILLEGRILLES_PATH
+
   sudo cp -R etc/* $MILLEGRILLES_ETC
   sudo cp -R bin/* $MILLEGRILLES_BIN
 
   sudo chown -R $MILLEGRILLES_USER_DEPLOYEUR:$MILLEGRILLES_GROUP $MILLEGRILLES_PATH
-  sudo chmod -R 2755 $MILLEGRILLES_PATH
 
   echo "[OK] $MILLEGRILLES_PATH pret"
 }
 
 # Execution de l'installation
-installer_docker
-preparer_comptes
-preparer_opt
+installer() {
+  installer_docker
+  preparer_comptes
+  preparer_opt
+}
+
+creer_millegrille() {
+  # Faire un hook vers la creation d'une millegrille si le parametre est inclus
+  if [ ! -z $NOM_MILLEGRILLE ]; then
+    sudo -u $MILLEGRILLES_USER_DEPLOYEUR $MILLEGRILLES_BIN/creer_millegrille.sh $NOM_MILLEGRILLE
+  else
+    echo
+    echo "[INFO] Installation de la base millegrilles completee, il faut maintenant creer votre millegrilles"
+    echo "[INFO] Utiliser le script: /opt/millegrilles/bin/creer_millegrille.sh NOM_NOUVELLE_MILLEGRILLE"
+    echo
+  fi
+}
+
+installer
+creer_millegrille
