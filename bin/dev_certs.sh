7426a8e7f251c0c85ab92776ed563aae2cc673cb
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-09-29 15:49:54 +0000

    Env dev pour monitor.

---
+++ bin/dev_certs.sh
@@ -0,0 +1,15 @@
+docker service create \
+  --name mon_secret \
+  --secret test1.pki.ca.passwords.20190929154156 \
+  --secret test1.pki.ca.root.cert.20190929154156 \
+  --secret test1.pki.ca.root.key.20190929154156 \
+  --secret test1.pki.ca.millegrille.cert.20190929154156 \
+  --secret test1.pki.ca.millegrille.key.20190929154156 \
+  --secret test1.passwd.python.maitredescles.json.20190929154156 \
+  --secret test1.pki.maitredescles.cert.20190929154156 \
+  --secret test1.pki.maitredescles.key.20190929154156 \
+  --secret test1.pki.maitredescles.key_cert.20190929154156 \
+  --secret test1.pki.ca.millegrille.fullchain.20190929154156 \
+  --mount type=bind,source=/home/mathieu/mgdev/certs,target=/opt/millegrilles/dist/ \
+  --entrypoint "sleep 10000" \
+  ubuntu
