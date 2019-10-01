57fb9a297e9eb946b546b207366c5359ddeea817
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-10-01 16:43:37 +0000

    Travail sur taches.

--- bin/dev_certs.sh
+++ bin/dev_certs.sh
@@ -1,15 +1,15 @@
 docker service create \
   --name mon_secret \
-  --secret dev3.pki.ca.passwords.20190930203653 \
-  --config dev3.pki.ca.root.cert.20190930203653 \
-  --secret dev3.pki.ca.root.key.20190930203653 \
-  --config dev3.pki.ca.millegrille.cert.20190930203653 \
-  --secret dev3.pki.ca.millegrille.key.20190930203653 \
-  --secret dev3.passwd.python.maitredescles.json.20190930203653 \
-  --config dev3.pki.maitredescles.cert.20190930203653 \
-  --secret dev3.pki.maitredescles.key.20190930203653 \
-  --secret dev3.pki.maitredescles.key_cert.20190930203653 \
-  --config dev3.pki.ca.millegrille.fullchain.20190930203653 \
+  --secret dev3.pki.ca.passwords.20191001134313 \
+  --config dev3.pki.ca.root.cert.20191001134313 \
+  --secret dev3.pki.ca.root.key.20191001134313 \
+  --config dev3.pki.ca.millegrille.cert.20191001134313 \
+  --secret dev3.pki.ca.millegrille.key.20191001134313 \
+  --secret dev3.passwd.python.maitredescles.json.20191001134313 \
+  --config dev3.pki.maitredescles.cert.20191001134313 \
+  --secret dev3.pki.maitredescles.key.20191001134313 \
+  --secret dev3.pki.maitredescles.key_cert.20191001134313 \
+  --config dev3.pki.ca.millegrille.fullchain.20191001134313 \
   --mount type=bind,source=/home/mathieu/mgdev/certs,target=/opt/millegrilles/dist/ \
   --entrypoint "sleep 10000" \
   ubuntu
