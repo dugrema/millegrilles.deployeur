c23113eff39234c8b238a1e276df333699d715df
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-10-10 19:18:53 +0000

    Creation nouveaux script demarrage pour noeud, service systemd.

---
+++ bin/noeud.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Demarrage du noeud"
+
+MG_CONFIG_JSON=/opt/millegrilles/etc/noeud_cles.json
+
+mkdir -p /var/log/millegrilles
+
+if [ ! -f MG_CONFIG_JSON ]; then
+  echo "Le fichier de configuration est manquant: "
+  exit 1
+fi
+
+export MG_CONFIG_JSON
+python3 /opy/millegrilles/bin/noeud.py "$@"
