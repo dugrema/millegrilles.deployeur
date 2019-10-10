1ebad5cd3dec16d7a985d6824d565b8f294b16e1
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-10-10 00:10:06 +0000

    Creation script pour noeud.

---
+++ bin/creer_csr_noeud.sh
@@ -0,0 +1,55 @@
+source /opt/millegrilles/etc/paths.env
+
+if [ -z $1 ]; then
+  echo "Il faut fournir le nom de la millegrille en parametre"
+  exit 1
+fi
+
+export NOM_MILLEGRILLE=$1
+
+REP_MILLEGRILLE=$MILLEGRILLES_PATH/$NOM_MILLEGRILLE
+CURDATE=`date +%Y%m%d%H%M%S`
+
+REP_CERTS=$REP_MILLEGRILLE/$MILLEGRILLES_CERTS
+REP_KEYS=$REP_MILLEGRILLE/$MILLEGRILLES_KEYS
+HOSTNAME=`cat /etc/hostname`
+
+creer_cert_noeud() {
+  # Params
+  # - TYPE_NOEUD: maitredescles, middleware, deployeur, noeud
+  # - EXTENSION: noeud_req_extensions, middleware_req_extensions
+  # - PASSWORD: si un mot de passe doit etre genere
+  set -e
+
+  mkdir -p $REP_CERTS
+  mkdir -p $REP_KEYS
+  chmod 700 $REP_KEYS
+
+  if [ -z $TYPE_NOEUD ]; then
+    TYPE_NOEUD=noeud
+  fi
+
+  if [ -z $EXTENSION ]; then
+    EXTENSION=noeud_req_extensions
+  fi
+
+  echo "[INFO] Creation certificat $TYPE_NOEUD"
+
+  KEY=$REP_KEYS/${NOM_MILLEGRILLE}_${TYPE_NOEUD}_${HOSTNAME}_${CURDATE}.key.pem
+  REQ=$REP_CERTS/${NOM_MILLEGRILLE}_${TYPE_NOEUD}_${HOSTNAME}_${CURDATE}.req.pem
+  SUBJECT="/O=$NOM_MILLEGRILLE/OU=$TYPE_NOEUD/CN=$HOSTNAME"
+
+  NOM_NOEUD= \
+  openssl req -newkey rsa:2048 -sha512 -nodes \
+              -config $MILLEGRILLES_OPENSSL_CNFMILLEGRILLES \
+              -out $REQ -outform PEM -keyout $KEY -keyform PEM \
+              -subj $SUBJECT
+
+  ln -sf $KEY $REP_KEYS/${NOM_MILLEGRILLE}_${TYPE_NOEUD}.key.pem
+
+  echo "[OK] Creation requete $TYPE_NOEUD complet"
+  echo "Coller la valeur suivante dans CoupDOeil / PKI / Signer un certificat de noeud"
+  cat $REQ
+}
+
+creer_cert_noeud
---
+++ install_noeud.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+# Utilisation de sudo pour faire entrer le mot de passe des le debut
+sudo echo "Installation d'un noeud de MilleGrilles"
+source etc/paths.env
+
+installer_autres_deps() {
+  # Random number gens hardware, pip3, avahi-daemon
+  sudo apt install -y rng-tools python3-pip avahi-daemon
+}
+
+installer_dependances() {
+  echo "[INFO] Installer deployeur Python et dependances"
+  MG_CONSIGNATION=MilleGrilles.consignation.python
+
+  set -e
+  mkdir -p tmp/
+  cd tmp/
+
+  # Installer MilleGrilles.consignation.python
+  if [ ! -d $MG_CONSIGNATION ]; then
+    git clone ssh://mathieu@repository.maple.mdugre.info/var/lib/git/$MG_CONSIGNATION
+  else
+    git -C $MG_CONSIGNATION pull
+  fi
+  cd $MG_CONSIGNATION
+  sudo pip3 install -r requirements.txt
+  sudo python3 setup.py install
+
+  # Fix bug 'cannot find abc'
+  sudo pip3 uninstall -y bson
+  sudo pip3 uninstall -y pymongo
+  sudo pip3 install pymongo
+  cd ../..
+
+  sudo pip3 install -r requirements.txt
+  sudo python3 setup.py install
+
+  # Installer script demarrage
+#  sudo cp etc/millegrilles.service /etc/systemd/system/millegrilles.service
+#  sudo chmod 644 /etc/systemd/system/millegrilles.service
+#  sudo systemctl enable millegrilles
+
+  echo "[OK] Deployeur Python et dependances installes"
+}
+
+preparer_opt() {
+  set -e  # Arreter execution sur erreur
+  echo "[INFO] Preparer $MILLEGRILLES_PATH"
+  sudo mkdir -p $MILLEGRILLES_BIN
+  sudo mkdir -p $MILLEGRILLES_ETC
+  sudo mkdir -p $MILLEGRILLES_CACERT
+
+  sudo chmod -R 2755 $MILLEGRILLES_PATH
+
+  sudo cp -R etc/* $MILLEGRILLES_ETC
+  sudo cp -R bin/* $MILLEGRILLES_BIN
+
+  echo "[OK] $MILLEGRILLES_PATH pret"
+}
+
+preparer_requete_csr() {
+  $MILLEGRILLES_BIN/creer_csr_noeud.sh
+}
+
+# Execution de l'installation
+installer() {
+  # Au besoin, preparer l'environnement du RPi avant le reste. Ajoute swapfile et autres dependances
+  preparer_rpi
+
+  installer_autres_deps
+  installer_deployeur
+
+  preparer_opt
+}
+
+preparer_rpi() {
+  ARCH=`uname -m`
+  if [ $ARCH == 'aarch64' ]; then
+    echo "Preparation speciale pour un RaspberryPi"
+
+    echo "[INFO] S'assurer que le swap est active - il faut au moins 1G de swap"
+    if [ ! -f /swapfile ]; then
+      sudo fallocate -l 1G /swapfile
+      sudo dd if=/dev/zero of=/swapfile bs=1024 count=1048576
+      sudo chmod 600 /swapfile
+      sudo mkswap /swapfile
+      sudo swapon /swapfile
+      echo "/swapfile  swap  swap  defaults  0 0" | sudo tee -a /etc/fstab
+    fi
+
+    # Pour RPi 64bit (pip requirement: lxml)
+    sudo apt install -y libxml2-dev libxmlsec1-dev python3-cffi
+  fi
+}
+
+installer
+creer_millegrille $1
