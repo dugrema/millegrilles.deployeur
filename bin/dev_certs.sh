0f0b8b2e115610c5efd0bc50aa1882cb26ba8707
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-10-11 01:17:28 +0000

    Fix queue config bug pour grosfichiers upload. Versions docker.

--- bin/dev_certs.sh
+++ bin/dev_certs.sh
@@ -1,15 +1,15 @@
 docker service create \
   --name mon_secret \
-  --secret dev3.pki.ca.passwords.20191001134313 \
-  --config dev3.pki.ca.root.cert.20191001134313 \
-  --secret dev3.pki.ca.root.key.20191001134313 \
-  --config dev3.pki.ca.millegrille.cert.20191001134313 \
-  --secret dev3.pki.ca.millegrille.key.20191001134313 \
-  --secret dev3.passwd.python.maitredescles.json.20191001134313 \
-  --config dev3.pki.maitredescles.cert.20191001134313 \
-  --secret dev3.pki.maitredescles.key.20191001134313 \
-  --secret dev3.pki.maitredescles.key_cert.20191001134313 \
-  --config dev3.pki.ca.millegrille.fullchain.20191001134313 \
+  --secret dev3.pki.ca.passwords.20191010233823 \
+  --config dev3.pki.ca.root.cert.20191010233823 \
+  --secret dev3.pki.ca.root.key.20191010233823 \
+  --config dev3.pki.ca.millegrille.cert.20191010233823 \
+  --secret dev3.pki.ca.millegrille.key.20191010233823 \
+  --secret dev3.passwd.python.maitredescles.json.20191010233823 \
+  --config dev3.pki.maitredescles.cert.20191010233823 \
+  --secret dev3.pki.maitredescles.key.20191010233823 \
+  --secret dev3.pki.maitredescles.key_cert.20191010233823 \
+  --config dev3.pki.ca.millegrille.fullchain.20191010233823 \
   --mount type=bind,source=/home/mathieu/mgdev/certs,target=/opt/millegrilles/dist/ \
   --entrypoint "sleep 10000" \
   ubuntu
