1df2118d59d8edc6d01159fa99ed0a0d94080300
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-10-11 22:46:00 +0000

    Ajout gestion version deployeur

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
 from setuptools import setup
+import subprocess
+
+
+def get_version():
+    try:
+        with open('version.txt', 'r') as version_file:
+            version = version_file.readline().strip()
+    except FileNotFoundError as e:
+        with open('build.txt', "r") as buildno_file:
+            build_no = buildno_file.read().strip()
+
+        commande_git_version = ['git', 'name-rev', '--name-only', 'HEAD']
+        output_process = subprocess.run(commande_git_version, stdout=subprocess.PIPE)
+        version = output_process.stdout.decode('utf8').strip()
+        version = '%s.%s' % (version, build_no)
+        print("Version: %s" % (version))
+
+    return version
 
 setup(
     name='MilleGrilles.deployeur',
-    version='1.14.1',
+    version='%s' % get_version(),
     packages=['mgdeployeur'],
     url='',
     license='',
