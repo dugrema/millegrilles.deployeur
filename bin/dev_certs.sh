6ab587d068c090af59aae4145cbc9ccd2e3c3096
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-10-25 19:03:00 +0000

    Ajout torf pour creer fichiers torrent.

--- bin/dev_certs.sh
+++ bin/dev_certs.sh
@@ -9,6 +9,8 @@ docker service create \
   --config dev3.pki.maitredescles.cert.20191010233823 \
   --secret dev3.pki.maitredescles.key.20191010233823 \
   --secret dev3.pki.maitredescles.key_cert.20191010233823 \
+  --config dev3.pki.nginx.fullchain.20191010233823 \
+  --secret dev3.pki.nginx.key.20191010233823 \
   --config dev3.pki.ca.millegrille.fullchain.20191010233823 \
   --mount type=bind,source=/home/mathieu/mgdev/certs,target=/opt/millegrilles/dist/ \
   --entrypoint "sleep 10000" \
