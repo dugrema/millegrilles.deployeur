c4d73a291271ab4b13f3ab42362ebd707c83c95a
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-11-19 17:31:23 +0000

    Script de desinstallation - reset.

---
+++ bin/desinstaller.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Supprimer millegrilles
+
+echo "[INFO] Desinstaller services systemd"
+sudo systemctl stop millegrilles
+sudo systemctl stop millegrilles.noeud
+sudo systemctl stop dockerIPv6mapper
+sudo rm /etc/systemd/system/millegrilles.service
+sudo rm /etc/systemd/system/millegrilles.noeud.service
+sudo rm /etc/systemd/system/dockerIPv6mapper.service
+sudo systemctl daemon-reload
+
+echo "[INFO] Desinstaller modules python"
+sudo pip3 uninstall -y millegrilles.deployeur
+sudo pip3 uninstall -y millegrilles.consignation.python
+
+echo "[INFO] Effacer /opt/millegrilles, logs, pids"
+sudo rm -rf /opt/millegrilles
+sudo rm -rf /var/log/millegrilles
+sudo rm -rf /run/millegrilles
+
+echo "[INFO] Desinstaller swarm docker"
+docker swarm leave --force
+docker volume prune -f
+
+echo
+echo "------------------------"
+echo "MilleGrilles desinstalle"
+echo
