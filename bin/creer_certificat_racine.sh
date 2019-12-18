2f034683d8d25e4acf34fc028048938d2b73550a
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-12-18 19:40:33 +0000

    Changement hash idmg de sha-1 a sha-2 512/224, toujours base58.

--- bin/creer_certificat_racine.sh
+++ bin/creer_certificat_racine.sh
@@ -10,7 +10,7 @@ TMP_FOLDER=$(pwd)/tmp
 echo "TMP_FOLDER=$TMP_FOLDER"
 
 extraire_fingerprint() {
-  FINGERPRINT=`openssl x509 -noout -fingerprint -in $1 | awk 'BEGIN{FS="="}{print $2}' | sed s/':'//g | tr '[:upper:]' '[:lower:]'`
+  FINGERPRINT=`openssl x509 -noout -fingerprint -sha512-224 -in $1 | awk 'BEGIN{FS="="}{print $2}' | sed s/':'//g | tr '[:upper:]' '[:lower:]'`
   IDMG=`$MILLEGRILLES_BIN/base58util.py encoder $FINGERPRINT`
   echo "Fingerprint de la millegrille : IDMG=$IDMG"
   export IDMG
