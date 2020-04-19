818fda6dac6adc25128f498bdf4f97a3dbc1a532
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-04-19 22:43:35 +0000

    Configuration domaines, ceduleur.

--- bin/init_docker.sh
+++ bin/init_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 CONFIG=../etc
-FICHIERS_CONFIG=( mongo mq transaction )
+FICHIERS_CONFIG=( mongo mq transaction maitrecles ceduleur domaines fichiers coupdoeilreact transmission )
 
 docker config rm docker.versions
 docker config create docker.versions $CONFIG/docker.versions.json
