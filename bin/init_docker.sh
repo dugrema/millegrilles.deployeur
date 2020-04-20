088f7cf7ffbf20afd2f7ce2dd4b30a0f9ad117f4
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-04-20 23:27:26 +0000

    Creation fichiers temporaires pour certs, key_cert.
    Permet d'eviter d'avoir a mapper les configs pour le service monitor

--- bin/init_docker.sh
+++ bin/init_docker.sh
@@ -2,12 +2,40 @@
 
 CONFIG=../etc
 FICHIERS_CONFIG=( mongo mq transaction maitrecles ceduleur domaines fichiers coupdoeilreact transmission )
+SERVICEMONITOR_VERSION=:x86_64_1.24.2
+REP_MILLEGRILLES=/var/opt/millegrilles
 
-docker config rm docker.versions
-docker config create docker.versions $CONFIG/docker.versions.json
+configurer() {
+  docker config rm docker.versions
+  docker config create docker.versions $CONFIG/docker.versions.json
 
-for MODULE in "${FICHIERS_CONFIG[@]}"; do
-  echo $MODULE
-  docker config rm docker.cfg.$MODULE
-  docker config create docker.cfg.${MODULE} $CONFIG/docker.${MODULE}.json
-done
+  for MODULE in "${FICHIERS_CONFIG[@]}"; do
+    echo $MODULE
+    docker config rm docker.cfg.$MODULE
+    docker config create docker.cfg.${MODULE} $CONFIG/docker.${MODULE}.json
+  done
+
+  sudo mkdir -p $REP_MILLEGRILLES
+  sudo chown mg_deployeur:millegrilles $REP_MILLEGRILLES
+}
+
+# Installer le service ServiceMonitor
+demarrer() {
+  docker service create \
+    --name service_monitor \
+    --mount type=bind,source=/run/docker.sock,destination=/run/docker.sock \
+    --mount type=bind,source=$REP_MILLEGRILLES,destination=/var/opt/millegrilles \
+    --user 1002:115 \
+    docker.maceroc.com/millegrilles_consignation_python_main${SERVICEMONITOR_VERSION} \
+    demarrer_servicemonitor.py --debug
+}
+
+debug() {
+  docker service create \
+    --name service_monitor \
+    ubuntu /bin/sleep 10000
+}
+
+configurer
+debug
+# demarrer
