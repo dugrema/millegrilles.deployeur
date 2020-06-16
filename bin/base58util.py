cf57a1c9c5128a439e558b03692b2050be7a38a1
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-06-16 19:09:00 +0000

    Encodage IDMG avec date exp certificat (epoch / 1000, bytes 28-32)

--- bin/base58util.py
+++ bin/base58util.py
@@ -2,7 +2,9 @@
 import argparse
 import base58
 import binascii
-
+import math
+import struct
+import datetime
 
 class Base58Util:
 
@@ -17,26 +19,54 @@ class Base58Util:
 
         self._parser.add_argument(
             '--type', type=str, nargs=1, choices=['hex'], default='hex',
-            help="Commande a executer: encoder, decoder"
+            help="Type d'encodage"
         )
 
-        self._parser.add_argument(
-            'commande', type=str, nargs=1, choices=['encoder', 'decoder'],
-            help="Commande a executer: encoder, decoder"
-        )
+        subparser_commandes = self._parser.add_subparsers(dest='commande', help='Commande')
 
-        self._parser.add_argument(
+        commande_encoder = subparser_commandes.add_parser('encoder', help='Encoder en base58')
+        commande_encoder.add_argument(
             'valeur', type=str, nargs=1,
             help='Valeur'
+        )
+
+        commande_decoder = subparser_commandes.add_parser('decoder', help='Decoder en base58')
+        commande_decoder.add_argument(
+            'valeur', type=str, nargs=1,
+            help='Valeur'
+        )
+
+        idmg_encoder = subparser_commandes.add_parser('idmg_enc', help='Encoder IDMG')
+        idmg_encoder.add_argument(
+            'valeur', type=str, nargs=1,
+            help='Valeur'
+        )
+        idmg_encoder.add_argument(
+            'date_end_cert', type=int,
+            help='Date certificat'
+        )
+
+        idmg_decoder = subparser_commandes.add_parser('idmg_dec', help='Decoder IDMG')
+        idmg_decoder.add_argument(
+            'valeur', type=str, nargs=1,
+            help='Valeur'
+        )
+        idmg_decoder.add_argument(
+            '--expiration', action='store_true',
+            help='Affiche la date d\'expiration du IDMG'
         )
 
         self._args = self._parser.parse_args()
 
     def executer(self):
-        if self._args.commande[0] == 'encoder' and self._args.type == 'hex':
+        if self._args.commande == 'encoder' and self._args.type == 'hex':
             self.encoder_hex()
-        elif self._args.commande[0] == 'decoder' and self._args.type == 'hex':
+        elif self._args.commande == 'decoder' and self._args.type == 'hex':
             self.decoder_hex()
+        elif self._args.commande == 'idmg_enc' and self._args.type == 'hex':
+            self.idmg_encoder()
+        elif self._args.commande == 'idmg_dec' and self._args.type == 'hex':
+            self.idmg_decoder()
         else:
             print("Commande: %s, type: %s" % (self._args.commande, self._args.type))
             self._parser.print_help()
@@ -50,6 +80,31 @@ class Base58Util:
         valeur = base58.b58decode(self._args.valeur[0])
         valeur_hex = binascii.hexlify(valeur).decode('utf-8')
         print(valeur_hex)
+
+    def idmg_encoder(self):
+        valeur = binascii.unhexlify(self._args.valeur[0])
+        date_epoch = self._extract_epoch(self._args.date_end_cert)
+        valeur_combinee = valeur + date_epoch
+        valeur_base58 = base58.b58encode(valeur_combinee).decode('utf-8')
+        print(valeur_base58)
+
+    def idmg_decoder(self):
+        valeur = base58.b58decode(self._args.valeur[0])
+        valeur_hex = binascii.hexlify(valeur[0:28]).decode('utf-8')
+        date_exp = struct.unpack('I', valeur[28:32])[0]
+        date_epoch_sec = date_exp * 1000
+
+        if self._args.expiration:
+            date_epoch_dt = datetime.datetime.fromtimestamp(date_epoch_sec)
+            print('Expiration IDMG : %s' % date_epoch_dt)
+        else:
+            print('%s;%d' % (valeur_hex, date_epoch_sec))
+
+    def _extract_epoch(self, date_epoch: int):
+        date_float = float(date_epoch)
+        date_int = math.ceil(date_float / 1000)
+        date_bytes = struct.pack('I', date_int)
+        return date_bytes
 
 
 if __name__ == '__main__':
--- bin/idmg.sh
+++ bin/idmg.sh
@@ -2,7 +2,9 @@
 
 extraire_fingerprint() {
   FINGERPRINT=`openssl x509 -noout -fingerprint -sha512-224 -in $1 | awk 'BEGIN{FS="="}{print $2}' | sed s/':'//g | tr '[:upper:]' '[:lower:]'`
-  IDMG=`./base58util.py encoder $FINGERPRINT`
+  DATE_CERT=`openssl x509 -noout -enddate -in $1 | cut -d= -f 2`
+  DATE_EPOCH=`date --date="$DATE_CERT" +%s`
+  IDMG=`./base58util.py idmg_enc $FINGERPRINT $DATE_EPOCH`
   echo "Fingerprint de la millegrille : IDMG=$IDMG"
   export IDMG
 }
