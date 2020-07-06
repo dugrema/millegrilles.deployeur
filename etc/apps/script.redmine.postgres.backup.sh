96387ab33cb64f970e94745c812d4a84efa0db0b
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-07-06 19:28:42 +0000

    Fix script db pour redmine postgres

--- etc/apps/script.redmine.postgres.backup.sh
+++ etc/apps/script.redmine.postgres.backup.sh
@@ -4,8 +4,10 @@ EXIT_CODE=0
 
 echo "[OK] Demarrage script de backup de redmine.postgres"
 
+PGPASSWORD=`cat /run/secrets/redmine-passwd`
+
 # Attente postgres
-pg_dump redmine -U redmine | xz - > /tmp/backup.redmine.postgres.xz
+pg_dump redmine -U redmine -h postgres | xz - > /tmp/backup/backup.redmine.postgres.xz
 EXIT_CODE=$?
 
 echo "[OK] Fin du script de backup de redmine.postgres"
