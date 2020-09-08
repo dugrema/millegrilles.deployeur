c21d527082a2618df1b01af1d1c2e535334b1487
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-09-08 23:40:36 +0000

    Ajout except pour verif bluetooth

--- acteur/BLELoader.py
+++ acteur/BLELoader.py
@@ -15,19 +15,24 @@ try:
     from acteur.BLEBaseClasses import find_adapter
     from gi.repository import GLib
     import dbus
-    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-    bus = dbus.SystemBus()
-    mainloop = GLib.MainLoop()
-    
-    adapter = find_adapter(bus)
-    
-    if not adapter:
-        logger_module.info('BLE adapter not found')
-    else:
-        ble_detecte = True
+
+    try:
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+        bus = dbus.SystemBus()
+        mainloop = GLib.MainLoop()
+
+        adapter = find_adapter(bus)
+
+        if not adapter:
+            logger_module.info('BLE adapter not found')
+        else:
+            ble_detecte = True
+    except dbus.exceptions.DBusException:
+        logger_module.exception("Erreur bluetooth")
 except ImportError as ie:
     logger_module.info("Erreur import bluetooth deps : %s", str(ie))
-
+except:
+    logger_module.exception("Erreur verification si bluetooth est supporte")
 
 def verifier_presence_bluetooth():
     logger = logging.getLogger(__name__ + '.verifier_presence_bluetooth')
