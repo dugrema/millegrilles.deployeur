354555be3ef999bc3b9cfc76f559a89fcb5064bd
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-10-26 23:20:28 +0000

    Restauration avec script

--- catalogues/applications/redmine_mariadb/script.redmine.mariadb.restore.sh
+++ catalogues/applications/redmine_mariadb/script.redmine.mariadb.restore.sh
@@ -4,8 +4,14 @@ EXIT_CODE=0
 
 echo "[OK] Demarrage script de restauration de redmine.mariadb"
 
+export PASSWORD=`cat /run/secrets/passwd.redmine`
+EXIT_CODE=$?
+if [ -z $PASSWORD ]; then
+  exit 1
+fi
+
 # export PGPASSWORD=`cat $REDMINE_DB_PASSWORD_FILE`
-xzcat /mnt/backup.redmine.mariadb.xz | mysql -h mariadb -u redmine -p"`cat /run/secrets/passwd.redmine`" redmine
+xzcat /mnt/backup.redmine.mariadb.xz | mysql -h mariadb -u redmine -p"$PASSWORD" redmine
 EXIT_CODE=$?
 
 echo "[OK] Fin du script de restauration de redmine.mariadb"
