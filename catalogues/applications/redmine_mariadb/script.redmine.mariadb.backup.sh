bfac444896b3d614df5428c39dc70be08bc0143d
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-10-26 18:06:57 +0000

    Retrait mapping /backup dans binding
    Pas necessaire, on fait le staging dans le container. Simplifie cleanup.

--- catalogues/applications/redmine_mariadb/script.redmine.mariadb.backup.sh
+++ catalogues/applications/redmine_mariadb/script.redmine.mariadb.backup.sh
@@ -7,7 +7,7 @@ echo "[OK] Demarrage script de backup de redmine.mariadb"
 export PGPASSWORD=`cat $REDMINE_DB_PASSWORD_FILE`
 
 # Attente postgres
-mysqldump -h mariadb -u redmine -p"`cat /run/secrets/redmine-passwd`" redmine | xz - > /tmp/backup/backup.redmine.mariadb.xz
+mysqldump -h mariadb -u redmine -p"`cat /scripts/redmine-passwd`" redmine | xz - > /backup/backup.redmine.mariadb.xz
 EXIT_CODE=$?
 
 echo "[OK] Fin du script de backup de redmine.mariadb"
