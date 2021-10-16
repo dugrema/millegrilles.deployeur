892fb9327bace8c8ef041cd24dd28b77ed6985fe
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-10-16 15:38:34 +0000

    Utilisation noeud_id pour dummy senseur

--- bin/redis_client.sh
+++ bin/redis_client.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker run -it --network millegrille_net --rm redis redis-cli -h redis
+IMAGE_REDIS=redis:6-alpine
+
+docker run -it --network millegrille_net --rm $IMAGE_REDIS redis-cli -h redis
