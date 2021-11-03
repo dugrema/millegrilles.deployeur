f407a84194da17247a2a57450797840e7f49a00d
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-11-03 11:53:45 +0000

    Refact command publish_mongo en publish_ports
    Utilise pour toutes les apps en dev

--- bin/dev/publish_mongo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-IMG_MONGO=$1
-
-docker service update --publish-add published=27017,target=27017,mode=host $IMG_MONGO
---
+++ bin/dev/publish_ports.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker service update -d --publish-add published=8380,target=8380,mode=host certissuer
+docker service update -d --publish-add published=27017,target=27017,mode=host mongo
+docker service update -d --publish-add published=10443,target=443,mode=host mongoexpress
+docker service update -d --publish-add published=6379,target=6379,mode=host redis
+docker service update -d --publish-add published=3001,target=443,mode=host web_protege
+docker service update -d --publish-add published=3003,target=443,mode=host web_coupdoeil
+docker service update -d --publish-add published=3021,target=443,mode=host fichiers
