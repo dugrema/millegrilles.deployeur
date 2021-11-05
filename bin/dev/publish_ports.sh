fbc99b6d618f9088dd5b1905b44c2e96e57a3f1b
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-11-05 12:57:36 +0000

    Retrait publication port mq 8443
    Utilise pour dev, publier comme mongo

--- bin/dev/publish_ports.sh
+++ bin/dev/publish_ports.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker service update -d --publish-add published=8380,target=8380,mode=host certissuer
+docker service update -d --publish-add published=8443,target=8443,mode=host mq
 docker service update -d --publish-add published=27017,target=27017,mode=host mongo
 docker service update -d --publish-add published=10443,target=443,mode=host mongoexpress
 docker service update -d --publish-add published=6379,target=6379,mode=host redis
