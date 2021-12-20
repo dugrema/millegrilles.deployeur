664a11100073c4ad6033bd60fa3a4da84686ece9
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-12-20 23:31:24 +0000

    mg-dev5 build

--- bin/start_monitor_test.sh
+++ bin/start_monitor_test.sh
@@ -2,9 +2,10 @@
 
 # export MG_CONSIGNATIONFICHIERS_HOST=mg-dev4
 # export MG_CONSIGNATIONFICHIERS_PORT=3021
-# export MG_MQ_HOST=mg-dev4
+export MG_MQ_HOST=mg-dev4
 export CERT_DUREE=14
 export CERT_DUREE_HEURES=0
+#export MGDEBUG_FORCE_RENEW=1
 
 python3 -m millegrilles.monitor.ServiceMonitor \
 --debug --dev --secrets /var/opt/millegrilles/secrets \
