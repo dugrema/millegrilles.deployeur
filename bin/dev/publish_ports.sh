ad9e128a22ce1e7777a4657c97281aea3e4e0d8e
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-03-02 17:38:33 -0500

    Ajout publish)_ports pour messagerie

--- bin/dev/publish_ports.sh
+++ bin/dev/publish_ports.sh
@@ -11,3 +11,4 @@ docker service update -d --publish-add published=3021,target=443,mode=host fichi
 docker service update -d --publish-add published=9200,target=9200,mode=host elasticsearch
 docker service update -d --publish-add published=3037,target=443,mode=host collections
 docker service update -d --publish-add published=3013,target=443,mode=host senseurspassifs_web
+docker service update -d --publish-add published=3039,target=443,mode=host messagerie_web
